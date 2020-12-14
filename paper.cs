7b406bbc35eecbeb0024ecbeadd4884a316ecb81
XiaoMing-2 <69342691+XiaoMing-2@users.noreply.github.com>
2020-12-14 17:38:02 +0800

    Add files via upload

---
+++ paper.cs
@@ -0,0 +1,19 @@
+using Terraria;
+using Treearia.ModLoader;
+using Terraria.ID;
+using Terraria.Localization;
+
+namespace xxxMod
+{
+	public class paper : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			
+		}
+		public override void SetDefault()
+		{
+			
+		}
+	}
+}
